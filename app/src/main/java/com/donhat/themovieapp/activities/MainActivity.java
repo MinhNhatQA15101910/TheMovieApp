7c9644cc96e90986e5801a65267c1350fd29a3bd
Do Nhat <dmnhat.pt@gmail.com>
2024-04-08 11:06:22 +0700

    Creating Data classes

--- app/src/main/java/com/donhat/themovieapp/activities/MainActivity.java
+++ app/src/main/java/com/donhat/themovieapp/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.donhat.themovieapp;
+package com.donhat.themovieapp.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.donhat.themovieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/donhat/themovieapp/models/Movie.java
@@ -0,0 +1,4 @@
+package com.donhat.themovieapp.models;
+
+public class Movie {
+}
